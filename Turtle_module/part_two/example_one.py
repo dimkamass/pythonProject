2ddebd85c9664448b9f187771d60aa619b5cbf94
Dmitriy M <lackandhite2@rambler.ru>
2021-09-11 14:47:16 +0300

    come and push

--- Turtle_module/part_two/example_one.py
+++ Turtle_module/part_two/example_one.py
@@ -1,14 +1,15 @@
 
-import turtle
-
-turtle.Screen().setup(400, 400)               # устанавливаем размер граф. окна
-turtle.Screen().addshape('rocketship.png')    # добавляем форму черепашки
-
-turtle.Screen().bgpic('space.jpg')            # устанавливаем фоновое изображение
-turtle.shape('rocketship.png')                # устанавливаем форму черепашки
-turtle.pencolor('green')
-turtle.pensize(5)
-
-for _ in range(4):
-  turtle.forward(150)
-  turtle.left(90)
+import turtle as t
+t.Screen().bgcolor('DeepSkyBlue')
+t.shape('turtle')
+t.stamp()
+for _ in range(12):
+  t.penup()
+  t.forward(70)
+  t.pendown()
+  t.forward(30)
+  t.penup()
+  t.forward(20)
+  t.stamp()
+  t.backward(120)
+  t.left(30)

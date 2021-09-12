29b92ef06b256915f1103d22adedcadcbd78b684
Dmitriy M <lackandhite2@rambler.ru>
2021-09-12 15:48:38 +0300

    come and push

--- Turtle_module/part_two/bear.py
+++ Turtle_module/part_two/bear.py
@@ -1,6 +1,38 @@
 import turtle as t
 
-head=50
-for _ in range(2):
-    t.circle(head)
-    head=head//2
+def up_down(x,y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+
+def bear_head():
+    head=60
+    for _ in range(2):
+        t.circle(head)
+        head=head//2
+
+def bear_nose_and_mouth():
+    up_down(0,30)
+    t.circle(10)
+    t.right(90)
+    t.forward(20)
+
+def eyes():
+    up_down(25,60)
+    t.dot(10,'black')
+    up_down(-25,60)
+    t.dot(10, 'black')
+
+def eyers():
+    up_down(50,100)
+    t.circle(15)
+    up_down(-50,100)
+    t.circle(15)
+
+def bear():
+    t.speed(10)
+    bear_head()
+    eyers()
+    bear_nose_and_mouth()
+    eyes()
+bear()
